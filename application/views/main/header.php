ce2c1ce042ce7013cdfb7c5ecf9c26d19febdff2
WonderfulLife2121 <nuralif34466@gmail.com>
2022-06-28 20:14:45 +0700

    menambah folder admin dan memisah css admin dengan visitor

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -10,22 +10,22 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
-    <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="<?= base_url() ?>assets/visitor/img/favicon.png" rel="icon">
+    <link href="<?= base_url() ?>assets/visitor/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/visitor/css/style.css" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: Personal - v4.7.0
@@ -55,7 +55,7 @@
                     <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
                     <li><a class="nav-link <?= $this->uri->segment(1) == 'contact' ? 'active' : '' ?> " href="<?= site_url('contact') ?>">Contact</a></li>
                     <li>
-                        <button type="button" class="btn btn-success">Login</button>
+                        <button type="button" class="btn btn-success"><a class="nav-link <?= $this->uri->segment(1) == 'login' ? 'active' : '' ?> " href="<?= site_url('login') ?>">Login</a></button>
                     </li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
